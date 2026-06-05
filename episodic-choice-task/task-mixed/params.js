bee34f560d67a6d22a483c0c5a0a663165fc07c0
Chris Iyer <chris.s.iyer@gmail.com>
2026-06-05 17:12:45 -0400

    mixed memorability old pilot

--- episodic-choice-task/task-mixed/params.js
+++ episodic-choice-task/task-mixed/params.js
@@ -1,5 +1,5 @@
 const params = {
-    experiment_id: "mixed_mem_A_pilot_6-5-26",
+    experiment_id: "mixed_memorability_6-5-26",
 
     // Trial structure (computed by buildSequencePlan; shown here for reference)
     //   78 H/H encoding + 78 L/L encoding + 78 old/old = 234 trials total
