3c3b6fddd61b3a3939ef4b99f64f3664b85c107c
dhalpern <djh2142@columbia.edu>
2026-08-12 09:28:33 -0400

    Update analysis.R

--- episodic-choice-task/analysis.R
+++ episodic-choice-task/analysis.R
@@ -47,6 +47,20 @@ sub_choice_plot_df |>
   theme_classic() +
   labs(x = "Old Value", y = "P(Old)", color = "Memorability")
 
+sub_choice_plot_df <- clean_df |>
+  group_by(participant_id, memorability_bin, old_value_fct, old_side) |>
+  summarize(
+    p_old_chosen = mean(old_chosen)
+  )
+sub_choice_plot_df |>
+  ggplot(aes(x = old_value_fct, y = p_old_chosen, 
+             color = memorability_bin, group = memorability_bin)) +
+  stat_summary(position = position_dodge(.2)) +
+  stat_summary(geom = "line", position = position_dodge(.2)) +
+  theme_classic() +
+  facet_wrap(vars(old_side), labeller = label_both) +
+  labs(x = "Old Value", y = "P(Old)", color = "Memorability")
+
 sub_rt_plot_df <- clean_df |>
   mutate(
     optimal_choice_str = if_else(optimal_choice == 1, "Correct", "Error")
@@ -63,6 +77,24 @@ sub_rt_plot_df |>
   stat_summary(geom = "line", position = position_dodge(.2)) +
   theme_classic() +
   # facet_wrap(vars(optimal_choice_str)) +
+  labs(x = "Old Value", y = "RT", color = "Memorability", linetype = "Response")
+
+sub_rt_plot_df <- clean_df |>
+  mutate(
+    optimal_choice_str = if_else(optimal_choice == 1, "Correct", "Error")
+  ) |>
+  filter_out(is.na(optimal_choice_str)) |>
+  group_by(participant_id, memorability_bin, old_value_fct, optimal_choice_str, old_side) |>
+  summarize(
+    rt = mean(rt)
+  )
+sub_rt_plot_df |>
+  ggplot(aes(x = old_value_fct, y = rt, 
+             color = memorability_bin, linetype = optimal_choice_str, group = interaction(memorability_bin, optimal_choice_str))) +
+  stat_summary(position = position_dodge(.2)) +
+  stat_summary(geom = "line", position = position_dodge(.2)) +
+  theme_classic() +
+  facet_wrap(vars(old_side), labeller = label_both) +
   labs(x = "Old Value", y = "RT", color = "Memorability", linetype = "Response")
 
 m1 <- glmer(old_chosen ~ old_value_c * memorability_bin + 
